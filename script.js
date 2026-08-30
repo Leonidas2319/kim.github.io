1bc79660eebe39e3be284bb1de3a74be3f857ab4
Leonid <LesnichiyForester@gmail.com>
2026-08-30 22:49:20 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const darkModeButton =
+    document.getElementById("darkModeButton");
+
+
+darkModeButton.addEventListener("click", function() {
+
+    document.body.classList.toggle("dark");
+
+});
